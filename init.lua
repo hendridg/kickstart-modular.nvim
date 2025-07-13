b70039020e5f773ffccd33a33cb9ce3e146c63d2
hendridg <hendridg@gmail.com>
2025-07-13 16:26:24 -0400

    feat: update init.lua for highlight
    Add config for include opacity in nvim config with ghostty

--- init.lua
+++ init.lua
@@ -105,5 +105,11 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+vim.cmd [[
+  highlight Normal ctermbg=none guibg=none
+  highlight NormalNC ctermbg=none guibg=none
+  highlight SignColumn ctermbg=none guibg=none
+  highlight VertSplit ctermbg=none guibg=none
+]]
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
