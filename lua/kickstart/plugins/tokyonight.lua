111f06e27a9fd020623fb5b759819e5b1b4ded28
hendridg <hendridg@gmail.com>
2025-10-03 09:20:28 -0400

    update(plugins): change default colorschema

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -10,9 +10,13 @@ return {
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-night'
       -- vim.cmd.colorscheme 'retrobox'
-      vim.cmd.colorscheme 'default'
+      -- vim.cmd.colorscheme 'default'
+      -- vim.cmd.colorscheme 'zellner'
+      -- vim.cmd.colorscheme 'quiet'
+      -- vim.cmd.colorscheme 'delek'
+      -- vim.cmd.colorscheme 'lunarperche'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=italic'
